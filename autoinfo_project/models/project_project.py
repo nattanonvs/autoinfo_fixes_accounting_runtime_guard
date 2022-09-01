dd37d68c8c10ca57f5331e1e700510ad77af209a
Varawit Termwaraporn <varawit@dataroot.asia>
2022-09-01 08:41:04 +0700

    [U] module autoinfo_project version 1.0 - Add job type sale in project for generate job number from salesperson

--- autoinfo_project/models/project_project.py
+++ autoinfo_project/models/project_project.py
@@ -9,7 +9,8 @@ class ProjectProject(models.Model):
     job_type = fields.Selection([
         ('job', 'Job'),
         ('pm', 'PM'),
-        ('service', 'Service')], string='Job Type', required=True, default='job')
+        ('service', 'Service'),
+        ('sale', 'Sales')], string='Job Type', required=True, default='job')
     project_department_id = fields.Many2one('hr.department', 'Department', help="Select Requested Department", required=True,
         default=lambda self: self._default_department_id())
     project_sale_order_ids = fields.One2many('sale.order', 'customer_project', string='Sale Orders', copy=False)
@@ -31,9 +32,9 @@ class ProjectProject(models.Model):
                 vals['job_no'] = department_id.project_no_pm_sequence_id.next_by_id(sequence_date=vals.get('date_start'))
             elif vals.get('job_type') == 'service' and department_id.project_no_service_sequence_id:
                 vals['job_no'] = department_id.project_no_service_sequence_id.next_by_id(sequence_date=vals.get('date_start'))
-        if (vals.get('job_no', '') == '' or not vals.get('job_no', '')) and vals.get('sale_person_id'):
-            sale_person_id = self.env['res.users'].sudo().browse(vals.get('sale_person_id'))
-            vals['job_no'] = sale_person_id.jobno_sequence_id.next_by_id(sequence_date=vals.get('date_start'))
+            elif vals.get('job_type') == 'sale' and vals.get('sale_person_id'):
+                sale_person_id = self.env['res.users'].sudo().browse(vals.get('sale_person_id'))
+                vals['job_no'] = sale_person_id.jobno_sequence_id.next_by_id(sequence_date=vals.get('date_start'))
         if (vals.get('job_no', '') == '' or not vals.get('job_no', '')):
             raise ValidationError('Please set sequence running for auto generate job number.')
         vals['name'] = vals['job_no'] + ':' + vals['name']
