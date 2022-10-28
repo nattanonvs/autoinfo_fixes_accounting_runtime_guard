59fbd65b5f88eb67552d22126add89a4fdc903cb
Varawit Termwaraporn <varawit@dataroot.asia>
2022-10-28 11:19:22 +0700

    [U] module autoinfo_purchase_form version 1.4 - Add field input for display in purchase order report

--- autoinfo_purchase_form/models/purchase_order.py
+++ autoinfo_purchase_form/models/purchase_order.py
@@ -5,3 +5,6 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     your_ref = fields.Char(string='Your Ref.')
+    delivery_to_report = fields.Text(string='Delivery To')
+    delivery_date_report = fields.Text(string='Delivery Date')
+    warranty_report = fields.Text(string='Warranty')
