292c25ea53bcd8e1fefd5c365c6a77554c9b3460
Jirawoot Akkaralikidkun <jirawoot@dataroot.asia>
2023-01-20 13:58:21 +0700

    module autoinfo_purchase_form version 1.12 - Fixed task A433-T89, A433-T90

--- autoinfo_purchase_form/__manifest__.py
+++ autoinfo_purchase_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Purchase Form',
-    'version': '1.11',
+    'version': '1.12',
     'category': 'DTR - ERP Purchase',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -15,6 +15,8 @@ Customize Report Purchase.
 
 Changelog
 ---------
+**Version 1.12**
+    - Fixed task A433-T89, A433-T90
 
 **Version 1.11**
     - Fixed style report from feedback customer
