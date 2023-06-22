214ea855a630ba2a476ee1e8c5a98ee57c658432
Pracha Phruekprasert <pracha@dataroot.asia>
2023-06-22 19:36:37 +0700

    Fix

---
+++ autoinfo_purchase_form/wizard/purchase_request_line_make_purchase_order.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
+    _inherit ='purchase.request.line.make.purchase.order'
+
+    def make_purchase_order(self):
+        vals = super(PurchaseRequestLineMakePurchaseOrder, self).make_purchase_order()
+        for item in self.item_ids:
+            request_id = item.line_id.request_id
+
+        for item in self.env['purchase.order'].browse(vals['domain'][0][2]):
+            item.dtr_purchase_delivery_to_report_id = request_id.dtr_purchase_delivery_to_report_id
+            item.delivery_to_report = request_id.delivery_to_report
+            item.delivery_date_report = request_id.delivery_date_report
+            item.dtr_purchase_warranty_report_id = request_id.dtr_purchase_warranty_report_id
+            item.dtr_purchase_term_report_id = request_id.dtr_purchase_term_report_id
+            item.dtr_purchase_shipment_report_id = request_id.dtr_purchase_shipment_report_id
+            item.dtr_purchase_weight_report_id = request_id.dtr_purchase_weight_report_id
+            item.shipping_standard = request_id.shipping_standard
+            item.tariff = request_id.tariff
+        return vals
