d8ba006719cfccb22b286a72e83e1914985f489b
Pracha Phruekprasert <pracha@dataroot.asia>
2023-06-23 17:32:44 +0700

    [u][autoinfo_purchase_form]v1.13b

--- autoinfo_purchase_form/__manifest__.py
+++ autoinfo_purchase_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Purchase Form',
-    'version': '1.13a',
+    'version': '1.13b',
     'category': 'DTR - ERP Purchase',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -16,7 +16,8 @@ Customize Report Purchase.
 Changelog
 ---------
 
-**Version 1.13-1.13a**
+**Version 1.13-1.13b**
+    - Add (FM-PU-04) Purchase Request Form
     - Task A433-T90, A433-T91, A433-T110, A21B-T7
 
 **Version 1.12**
