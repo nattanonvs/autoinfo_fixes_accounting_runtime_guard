23b8b050ca91eab64c3f62c13fed9bf54a15a498
Pracha Phruekprasert <pracha@dataroot.asia>
2023-07-20 10:45:48 +0700

    [u][autoinfo_accounting_form]v1.11i

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Account Form',
-    'version': '1.11h',
+    'version': '1.11i',
     'category': 'DTR - ERP Accounting',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -18,7 +18,7 @@ Customize Report Accounting.
 Changelog
 ---------
 
-**Version 1.11-1.11h**
+**Version 1.11-1.11i**
     - Task A21B-T17, A21B-T26, A21B-T33, A21B-T36, A21B-T47, A21B-T60, A21B-T59
     - Fixed wrong wording in invoice report, billing report, prepayment report and debit note report
 
