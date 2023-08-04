607b80be0fdc9dbabced961f85f9ba98145004f9
Jirawoot Akkraralikidkun <jirawoot@dataroot.asia>
2023-08-04 09:50:38 +0700

    module autoinfo_account version 1.2

--- autoinfo_account/models/account_move.py
+++ autoinfo_account/models/account_move.py
@@ -5,10 +5,4 @@ class AutoInfoAccountMove(models.Model):
     _inherit = 'account.move'
 
     cancel_reason = fields.Char(string='Cancel Reason', readonly=True, copy=False)
-    dtr_customer_ref = fields.Char(string='Customer Reference', copy=False)
-
-    def _post(self, soft=True):
-        for rec in self.filtered(lambda x: x.name == '/'):
-            if rec.dtr_customer_ref:
-                rec.write({'name': rec.dtr_customer_ref})
-        return super(AutoInfoAccountMove, self)._post(soft)
+    
