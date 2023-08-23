b4add9c623a86f24cae3bf2e1a12ea4e1e72ee10
Varawit Termwaraporn <varawit@dataroot.asia>
2023-08-23 15:24:09 +0700

    module autoinfo_accounting_form version 1.16 - Fixed issue display amount total in footer is null

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Account Form',
-    'version': '1.15',
+    'version': '1.16',
     'category': 'DTR - ERP Accounting',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -17,6 +17,9 @@ Customize Report Accounting.
 
 Changelog
 ---------
+
+**Version 1.16**
+    - Fixed issue display amount total in footer is null
 
 **Version 1.15**
     - Fixed issue display amount total in footer case down payment
