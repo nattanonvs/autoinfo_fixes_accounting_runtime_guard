54e9bfabc05cdda9e76db2293a500ca3d527dea3
Jirawoot Akkraralikidkun <jirawoot@dataroot.asia>
2023-09-11 11:59:34 +0700

    module autoinfo_accounting_form version 1.20 - Fix task A21B-T127

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Account Form',
-    'version': '1.19',
+    'version': '1.20',
     'category': 'DTR - ERP Accounting',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -17,6 +17,8 @@ Customize Report Accounting.
 
 Changelog
 ---------
+**Version 1.20**
+    - Fix task A21B-T127
 
 **Version 1.19**
     - Fixed issue deduct deposit payment line show in report
