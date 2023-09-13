3f8f0b35ef4f906e80a3cdba959b88eaee47b20e
Pracha Phruekprasert <pracha@dataroot.asia>
2023-09-13 18:14:19 +0700

    Fix

--- autoinfo_hr_expense/models/account_payment_register.py
+++ autoinfo_hr_expense/models/account_payment_register.py
@@ -5,3 +5,9 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
     wht_ids = fields.Many2many('dtr.wht', 'account_payment_register_wht_rel', string='Withholding Taxes')
+
+    def _create_payment_vals_from_wizard(self):
+        # OVERRIDE
+        payment_vals = super()._create_payment_vals_from_wizard()
+        payment_vals.update({'payment_wht_ids': [(4, wht.id, None) for wht in self.wht_ids]})
+        return payment_vals
