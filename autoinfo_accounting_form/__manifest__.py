769d4c2748f1061acc8c84a9a175e3482426fc1e
Jirawoot Akkraralikidkun <jirawoot@dataroot.asia>
2023-09-19 11:10:39 +0700

    module autoinfo_accounting_form version 1.21 - Fix task A21B-T147 (Prepayment Form)

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Account Form',
-    'version': '1.20',
+    'version': '1.21',
     'category': 'DTR - ERP Accounting',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -17,6 +17,9 @@ Customize Report Accounting.
 
 Changelog
 ---------
+**Version 1.21**
+    - Fix task A21B-T147 (Prepayment Form)
+
 **Version 1.20**
     - Fix task A21B-T127
 
