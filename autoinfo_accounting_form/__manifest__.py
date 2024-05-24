71db7ebb1d1f2c5f9602a5778a8a7cc2df84360f
Pracha Phruekprasert <pracha@dataroot.asia>
2024-05-24 16:55:05 +0700

    Fix

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -155,7 +155,6 @@ Changelog
     'application': False,
     'data': [
         'security/ir.model.access.csv',
-        'delete.xml',
         'reports/autoinfo_invoice_form.xml',
         'reports/autoinfo_tax_invoice_pp_sale_form.xml',
         'reports/autoinfo_tax_invoice_pp_form.xml',
