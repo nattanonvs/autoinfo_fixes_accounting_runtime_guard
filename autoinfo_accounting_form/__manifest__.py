50260786ecaf970e9816168328cdb4a10eefa12d
Jirawoot Akkraralikidkun <jirawoot@dataroot.asia>
2024-06-20 16:26:24 +0700

    module autoinfo_accounting_form version 1.40 - Fix form Customer Invoice Preprint (autoinfo) งานบริการ(task: A21B-T317)     - Fix form Customer Invoice Preprint (autoinfo) งานขาย (task: A21B-T317)

--- autoinfo_accounting_form/__manifest__.py
+++ autoinfo_accounting_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AUTO INFO - Account Form',
-    'version': '1.39',
+    'version': '1.40',
     'category': 'DTR - ERP Accounting',
     'license': 'OPL-1',
     'website': 'https://www.dataroot.asia/support-odoo',
@@ -17,6 +17,10 @@ Customize Report Accounting.
 
 Changelog
 ---------
+**Version 1.40**
+    - Fix form Customer Invoice Preprint (autoinfo) งานบริการ(task: A21B-T317)
+    - Fix form Customer Invoice Preprint (autoinfo) งานขาย (task: A21B-T317)
+
 **Version 1.39**
     - Fix form Customer Invoice Preprint (autoinfo) งานบริการ(task: A21B-T314)
     
